b432b3850dec3c06a94d4d2109809781b723da02
agungrambujana <agungrambujana@gmail.com>
2026-04-28 15:08:53 +0700

    mavproxy

---
+++ mavproxy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Exit immediately if a command fails
+
+set -e
+
+# Navigate to project directory
+
+cd ~/Desktop/drone
+
+# Activate virtual environment
+
+source venv/bin/activate
+
+# Run MAVProxy
+
+python venv/bin/mavproxy.py --master=/dev/ttyAMA0 --baudrate 1500000 --out=udp:192.168.137.1:14555 --out=udp:127.0.0.1:14550 --out=udp:127.0.0.1:14551
